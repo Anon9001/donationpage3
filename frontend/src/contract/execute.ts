d208528ca55e566edd7ea47a1ed269de9937827b
Anon 9001 <blueberrytom95@gmail.com>
2022-12-02 17:16:01 -0500

    correct improper whitelist token of usdc

--- frontend/src/contract/execute.ts
+++ frontend/src/contract/execute.ts
@@ -29,7 +29,7 @@ const _exec = (msg: any, totalLunaAmt: Number) =>
 
     if (totalLunaAmt > 0) {
       //testnet denom: ibc/F91EA2C0A23697A1048E08C2F787E3A58AC6F706A1CD2257A504925158CFC0F3
-      //mainnet denom: ibc/CBF67A2BCF6CAE343FDF251E510C8E18C361FC02B23430C121116E0811835DEF
+      //mainnet denom: ibc/B3504E092456BA618CC28AC671A71FB08C6CA0FD0BE7C8A5B5A3E2DD933CC9E4 
       const c2 = new Coin("ibc/CBF67A2BCF6CAE343FDF251E510C8E18C361FC02B23430C121116E0811835DEF", totalLunaAmt.valueOf()); // .0015 USDC
       msgExeCon =         new MsgExecuteContract(
         wallet.walletAddress,
